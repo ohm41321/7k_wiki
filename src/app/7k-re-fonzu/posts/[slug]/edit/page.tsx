eb77a7c04a84f0ebfa42051389aed84d4216e9ea
ohm41321 <ohm.roexe1488@gmail.com>
2025-10-01 23:28:56 +0700

    FZ

--- src/app/7k-re-fonzu/posts/[slug]/edit/page.tsx
+++ src/app/7k-re-fonzu/posts/[slug]/edit/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/lib/auth";
 import { getPostBySlug } from "@/app/lib/posts";
 import EditPostForm from "@/app/components/EditPostForm";
 
