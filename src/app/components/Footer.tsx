c9b43b0e1894c2719ae1b014f7d0293143e4e399
ohm41321 <ohm.roexe1488@gmail.com>
2025-10-03 22:54:42 +0700

    add contact, font_awesome

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 export default function Footer() {
   return (
     <footer className="bg-[#061018] border-t border-yellow-900/10 text-textLight">
@@ -7,8 +11,14 @@ export default function Footer() {
         <div>
           <h4 className="font-bold text-yellow-300">Fonzu Wiki</h4>
         </div>
-        <div className="text-sm">
-          <p>Contact: <a className="text-yellow-300" href="mailto:athitfkm@gmail.com">athitfkm@gmail.com</a></p>
+        <div className="text-sm flex items-center gap-4">
+          <a href="mailto:athitfkm@gmail.com" className="text-yellow-300 hover:text-yellow-400 flex items-center gap-2">
+            <FontAwesomeIcon icon={faEnvelope} size="lg" />
+            athitfkm@gmail.com
+          </a>
+          <a href="https://github.com/ohm41321" target="_blank" rel="noopener noreferrer" className="text-yellow-300 hover:text-yellow-400">| &nbsp;
+            <FontAwesomeIcon icon={faGithub} size="lg" /> ohm41321
+          </a>
         </div>
         <div className="text-sm text-gray-400">© {new Date().getFullYear()} Fonzu Wiki. All rights reserved.</div>
       </div>
