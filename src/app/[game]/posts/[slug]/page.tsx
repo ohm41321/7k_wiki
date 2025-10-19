2ff0af73b7f3eefe73a7f272d10b70e08fbaae31
ohm41321 <ohm.roexe1488@gmail.com>
2025-10-19 16:28:06 +0700

    fix_preview_img

--- src/app/[game]/posts/[slug]/page.tsx
+++ src/app/[game]/posts/[slug]/page.tsx
@@ -10,17 +10,56 @@ type Props = {
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const post = await getPostBySlug(params.slug);
+   const post = await getPostBySlug(params.slug);
 
-  if (!post || post.game !== params.game) {
-    return notFound();
-  }
+   if (!post || post.game !== params.game) {
+     return notFound();
+   }
 
-  return {
-    title: post.title,
-    description: post.content.substring(0, 150),
-  };
-}
+   // Get the first image for preview, or use default
+   const previewImage = post.imageurls && post.imageurls.length > 0
+     ? post.imageurls[0]
+     : '/pic/7k_banner.webp';
+
+   // Clean description for meta tags
+   const cleanDescription = post.content
+     ? post.content.replace(/[#*`~]/g, '').substring(0, 200) + '...'
+     : 'อ่านบทความและข่าวสารเกมกาชาล่าสุดจาก Fonzu Wiki';
+
+   const baseUrl = 'https://fonzu-wiki.vercel.app';
+   const postUrl = `${baseUrl}/${post.game}/posts/${post.slug}`;
+
+   return {
+     title: `${post.title} | Fonzu Wiki`,
+     description: cleanDescription,
+     openGraph: {
+       title: `${post.title} | Fonzu Wiki`,
+       description: cleanDescription,
+       url: postUrl,
+       siteName: 'Fonzu Wiki',
+       locale: 'th_TH',
+       type: 'article',
+       images: [
+         {
+           url: previewImage,
+           width: 1200,
+           height: 630,
+           alt: post.title,
+         },
+       ],
+     },
+     twitter: {
+       card: 'summary_large_image',
+       title: `${post.title} | Fonzu Wiki`,
+       description: cleanDescription,
+       images: [previewImage],
+       creator: '@fonzuwiki',
+     },
+     alternates: {
+       canonical: postUrl,
+     },
+   };
+ }
 
 export default async function PostPage({ params }: { params: { slug: string; game: string } }) {
   const post = await getPostBySlug(params.slug);
